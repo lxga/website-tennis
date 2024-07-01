fe163c93e894e2c0b74fd2d5fc70d22e9e5d822f
lxga <luca43gavrilas@gmail.com>
2024-07-01 17:41:58 +0300

    prostie

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const sw = document.getElementById("theme-tog gle");
+sw.addEventListener("click",() => {document.documentElement.classList.toggle('dark'); });
+//document.documentElement.classList.remove('dark');
